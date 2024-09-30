8d348a59639f7cfbb889404fb6e56db66d7368f3
MarcM <marina.ved80gmail.com>
2024-09-30 11:05:37 -0600

    add controllers

--- models/Thought.js
+++ models/Thought.js
@@ -5,7 +5,7 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = new Schema(
   {
     reactionId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       default: () => new mongoose.Types.ObjectId() 
     },
     reactionBody: {
