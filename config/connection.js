9fc00ec5863ac0e6f1981e40ae506a06667df566
MarcM <marina.ved80gmail.com>
2024-09-30 11:17:33 -0600

    new DB

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mygroceryDB');
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkCh18');
 
 module.exports = mongoose.connection;
