512349c936068a216f6448b77ff040dd492b1148
MarcM <marina.ved80gmail.com>
2024-09-30 12:37:09 -0600

    add put and delete to user

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,5 +39,45 @@ module.exports = {
       } catch (err) {
         res.status(500).json(err);
       }
-    }
+    },
+
+    // update a user
+    async updateUser(req, res) {
+      try {
+        const user = await User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $set: req.body },
+          { 
+            runValidators: true, 
+            new: true }
+        );
+  
+        if (!user) {
+          return res.status(404).json({
+            message: 'No user with that ID to update',
+          })
+        }
+  
+        res.json(user);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    },
+
+    // delete a user
+    async deleteUser(req, res) {
+      try {
+        const user = await User.findOneAndDelete({ _id: req.params.userId });
+  
+        if (!user) {
+          return res.status(404).json({ message: 'No user with that ID to delete' });
+        }
+  
+        res.json({ message: 'User successfully deleted' });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    },
 };
